168643394c7b59cf6dc9950b97d5616a31d4c348
Ritanshi-arch <112924759+Ritanshi-arch@users.noreply.github.com>
2022-10-31 22:14:12 +0530

    Change

--- index.js
+++ index.js
@@ -1,67 +1,48 @@
+var readlinesync = require("readline-sync")
 
 
-var readlineSync = require("readline-sync");
-var userName = readlineSync.question("what is your name: "); //defining global variable to access userName in total score message
+console.log("Welcome to the quiz world of Ritanshi!... Share the screenshot of your score")
+score = 0
+console.log("_________")
 
+var userName = readlinesync.question("May I know your good name ? ")
+console.log("-------------")
+console.log("Welcome " + userName + " , Nice pleasure to see you here. Participate in the QUIZ WORLD OF MINE. Wish you good luck ")
+console.log("-----")
 
-var score = 0;
-
-var questions = [{
-  question: "Where do i live? :",
-  answer: "Lucknow"
-}, {
-  question: "What is my favorite food? :",
-  answer: "Pizza"
-}, {
-  question: "Am i cat or a dog person? :",
-  answer: "Cat"
-}, {
-  question: "Which phone do i use? :",
-  answer: "Google pixel"
-}, {
-  question: "If not a web developer, what do you think i wanted to become?(hint: i did this as part-time during past years :",
-  answer: "Teacher"
-}
-];
-
-function welcome() {
-  console.log("welcome " + userName + " to the game, do you know Amaan! ");
-}
-
+console.log("Special Note, kindly use lower letter otherwise your answer get wrong!")
 
 function play(question, answer) {
-  var userAns = readlineSync.question(question);
+  var userAnswer = readlinesync.question(question);
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) {
+    console.log("Absolutely Correct!!!!! ")
+    score = score + 1
 
-  if (userAns.toUpperCase() === answer.toUpperCase()) {
-    console.log("Right");
-    score = score + 1;
   }
   else {
-    console.log("Wrong");
+    console.log("Alaas!!!! ")
+
   }
-  console.log("current score: ", + score);
-  console.log("---------------");
+  console.log(score)
+
 }
 
+play("what is my favourite color ? ", "black");
+play("what is my favourite movie? ", "harry potter");
+play("what is my favourite bike? ", "ninja");
+play("what is my favourite game? ", "bgmi")
+play("where do I live? ", "delhi")
 
 
 
-function game() {
-  for (var i = 0; i < questions.length; i++) {
-    var gameQuestion = questions[i];
-    play(gameQuestion.question, gameQuestion.answer)
-  }
-}
 
-function totalscore() {
-  if (score >= 3) {
-    console.log("Well played " + userName + " your score is " + score);
-  }
-  else {
-    console.log("better luck next time " + userName + " your score is " + score);
-  }
-}
 
-welcome();
-game();
-totalscore();
+
+console.log("-----------")
+var answer1 = readlinesync.question("Who is my favourite person? ")
+console.log(" YOU " + userName + " WILL ALWAYS BE MY FAVOURITE ")
+
+
+console.log("---------")
+console.log("your score final score is " + score)
+console.log("I appreciate your involvement in this project")
